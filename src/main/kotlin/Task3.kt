95b287af237358fd35de04d9ff9aa1741d6477f2
seif <saly@edu.hse.ru>
2023-12-02 15:10:11 +0300

    03-RemoveDuplicates

--- src/main/kotlin/Task3.kt
+++ src/main/kotlin/Task3.kt
@@ -1,7 +1,15 @@
 fun main() {
+    print("Enter Numbers:")
     val userInput : String = readln()
-    val names = userInput.split(" ").toTypedArray()
-    val lastElement = names.last()
-    println(lastElement)
-    println()
+    val arrayString = userInput.split(" ").toTypedArray()
+    val arrayNumbers = arrayString.map { it.toInt() }
+    val uniqueNumbers = arrayNumbers.distinct()
+    var numberOfUnique = 0
+    for(i in uniqueNumbers.indices){
+        numberOfUnique += 1
+    }
+    println(numberOfUnique)
+
+
+
 }
