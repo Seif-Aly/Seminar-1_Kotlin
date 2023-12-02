c26342798769bd582351037333c6e62299ae6484
seif <saly@edu.hse.ru>
2023-12-02 15:08:17 +0300

    02-Echo

--- src/main/kotlin/Task2.kt
+++ src/main/kotlin/Task2.kt
@@ -1,10 +1,9 @@
 fun main() {
-    print("Enter Name:")
+    print("Enter Names:")
     val userInput : String = readln()
     val names = userInput.split(" ").toTypedArray()
     val lastElement = names.last()
     for (i in 1..3) {
         println(lastElement)
     }
-
 }
